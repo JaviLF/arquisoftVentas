1419ad34aea135fb127cdb8adc037882b8e5107b
Javier Loayza Flores <javo_tralll@hotmail.com>
2020-02-24 17:02:02 -0400

    tests para calcular total de ventas

--- src/VentasTest.java
+++ src/VentasTest.java
@@ -13,5 +13,14 @@ class VentasTest {
 		Assert.assertEquals("Cantidad no disponible", venta1.agregarProducto(product1,4));
 		Assert.assertEquals("Producto agregado", venta1.agregarProducto(product1,2));
 	}
-
+	
+	@Test
+	void totalVentasTest() {
+		Product product1= new Articulo("Cocoa",5,3);
+		Product product2= new Servicio("Reparacion",6,12);
+		Venta venta1=new Venta();
+		venta1.agregarProducto(product1,2);
+		venta1.agregarProducto(product2,10);
+		Assert.assertEquals(190, venta1.totalVenta());
+	}
 }
